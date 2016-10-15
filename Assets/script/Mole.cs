6fefbfeb0cd9aecbe1b1699c00c5e666d44e418d
jiangxue0312 <jiangxue0312@gmail.com>
2016-10-15 12:01:07 +1100

    moles

---
+++ Assets/script/Mole.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour {
+
+    private Animator animator;
+    private bool showflag = false;
+    
+    
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (showflag)
+        {
+            GameObject moles = GameObject.Find("Moles");
+            MoleManager t = moles.GetComponent<MoleManager>();
+            float timeHide = t.HideDelay;
+            StartCoroutine(Hide(timeHide));
+        }
+	
+	}
+
+
+    public void Show()
+    {
+        if (showflag) return;
+        animator.Play("show");
+        showflag = true;
+    }
+
+    IEnumerator Hide(float timeHide)
+    {
+        showflag = false;
+        yield return new WaitForSeconds(timeHide);
+        animator.Play("hide");
+    }
+}
---
+++ Assets/script/MoleManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoleManager : MonoBehaviour {
+
+    // Use this for initialization
+    // List of the Mole 
+    public List<GameObject> moleList = new List<GameObject>();
+    // Time to wait before start the game, start to go time
+    public float StartDelay;
+    // Time before showing the mole 
+    public float ShowDelay;
+    // Time before hiding the mole
+    public float HideDelay;
+
+    // Maximum number of moles show in one term
+    //public int MaxMole;
+
+	void Start () {
+        InvokeRepeating("MoleShow", StartDelay, ShowDelay);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void MoleShow()
+    {
+        moleList[Random.Range(0, moleList.Count)].GetComponentInChildren<Mole>().Show();
+    }
+}
